c71cc448872e4b909ce8703d1ae5dae093c94ec2
Mai Cong Danh 19021233 <62579415+wowyman@users.noreply.github.com>
2020-09-23 01:10:33 +0700

    23-9

--- Dictionary/src/MainProgram.java
+++ Dictionary/src/MainProgram.java
@@ -21,6 +21,28 @@ public class MainProgram
         d.addWord("Eat","An");
         d.addWord("SuperNenechii","Strong");
         DictionaryCommandline dc = new DictionaryCommandline();
+        /* test function LookUp,removeWord,ExportToFile (23-9)
+        d.removeWord("Eat");
+
+
+        DictionaryCommandline dc = new DictionaryCommandline();
+
+
+        dc.dictionaryExportToFile(d,"C:\\Users\\pc\\OneDrive\\Máy tính\\BTL\\dictionary.txt");
         dc.showAllWords(d);
+        DictionaryManagement w = new DictionaryManagement();
+        int i= w.dictionaryLookup(d.wordArrayList,"Kill");
+        if(i != -1) {
+            System.out.println("\nTest dictionaryLookUp\n");
+            System.out.printf("%-10s%-25s%-25s\n", "No", "|English", "|Vietnamese");
+            System.out.printf("%-10d%-25s%-25s\n", i + 1, "|" + d.wordArrayList.get(i).getText()
+                    , "|" + d.wordArrayList.get(i).getDefinition());
+        }
+        else{
+            System.out.print("Word Not Found!");
+        }
+
+         */
+        
     }
 }
