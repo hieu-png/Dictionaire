366ffee15a21bb59b2bfa01d2c076d5724fba812
Mai Cong Danh 19021233 <62579415+wowyman@users.noreply.github.com>
2020-10-01 10:07:18 +0700

    1/10/20

--- Dictionary/src/DictionaryApplication.java
+++ Dictionary/src/DictionaryApplication.java
@@ -1,8 +1,9 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
@@ -21,6 +22,18 @@ public class DictionaryApplication {
     public void prepareGUI() {
         mainFrame = new JFrame("Dictionary 2.0");
         mainFrame.setSize(800, 600);
+        mainFrame.getContentPane().setBackground(Color.RED);
+        Image image = null;
+        try {
+            image = ImageIO.read(new File("C:\\Users\\pc\\OneDrive\\" +
+                    "Máy tính\\beautiful blue color background\\image.png"))
+                    .getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mainFrame.setContentPane(new ImagePanel(image));
+
+
 
         mainFrame.setLayout(null);
         mainFrame.addWindowListener(new WindowAdapter() {
@@ -29,47 +42,119 @@ public class DictionaryApplication {
             }
         });
 
+
         controlPanel = new JPanel();
         controlPanel.setLayout(null);
         mainFrame.add(controlPanel);
         mainFrame.setVisible(true);
+
+
     }
 
-    public void showExample() throws IOException {
-        final JTextField searchBar = new HintTextField("Search...");
+    public void UI(){
+
+        mainFrame.setLocationRelativeTo(null);
+        JTextArea searchBar = new HintTextArea("Search...");
 
         //JLabel label = new JLabel("ssssss");
         //mainFrame.add(label);
         //label.setBounds(20,40,100,30);
 
-        Font f = new Font("Arial", Font.BOLD, 14);
+        Font f = new Font("Arial", Font.BOLD, 20);
         searchBar.setFont(f);
         mainFrame.add(searchBar);
-        searchBar.setBounds(15,10,700,40);
+        searchBar.setBounds(15, 10, 700, 40);
 
-        Image image = ImageIO.read(new File("C:\\Users\\pc\\OneDrive\\" +
-                "Máy tính\\beautiful blue color background\\book.png"))
-                .getScaledInstance(18,18,Image.SCALE_DEFAULT);
-        JButton showAllWord = new JButton();
-        showAllWord.setIcon(new ImageIcon(image));
-
-
-        mainFrame.add(showAllWord);
-        showAllWord.setBounds(0,50,28,28);
-
-
-
-
+        Image image = null;
+        try {
+            image = ImageIO.read(new File("C:\\Users\\pc\\OneDrive\\" +
+                    "Máy tính\\beautiful blue color background\\book.png"))
+                    .getScaledInstance(18, 18, Image.SCALE_DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JButton ShowAllWordButton = new JButton();
+        assert image != null;
+        ShowAllWordButton.setIcon(new ImageIcon(image));
 
 
+        mainFrame.add(ShowAllWordButton);
+        ShowAllWordButton.setBounds(0, 50, 28, 28);
 
+
+        JButton OpenAddWordButton = new JButton("Add+");
+        mainFrame.add(OpenAddWordButton);
+        OpenAddWordButton.setBounds(200, 50, 80, 28);
+
+        OpenAddWordButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AddWordWindow();
+
+            }
+        });
 
 
         mainFrame.setVisible(true);
     }
 
-    public static void main(String[] args) throws IOException {
+
+
+    //------------function display AddWordWindow--------------------------------
+
+
+    public static void AddWordWindow(){
+        JFrame AddWordFrame = new JFrame("Add new word");
+        AddWordFrame.setSize(600, 400);
+
+        JPanel midControl = new JPanel();
+        midControl.setSize(400, 300);
+        midControl.setLayout(new BoxLayout(midControl, BoxLayout.Y_AXIS));
+
+
+        //AddWordFrame.getContentPane().setBackground(Color.RED);
+        //AddWordFrame.setLayout(null);
+//        AddWordFrame.addWindowListener(new WindowAdapter() {
+//            @Override
+//            public void windowOpened(WindowEvent e) {
+//            }
+//        });
+
+        Font font = new Font("Arial", Font.BOLD, 14);
+        JTextField AddBar = new HintTextField("Add...");
+
+        midControl.add(AddBar);
+        AddBar.setFont(font);
+        AddBar.setBounds(15, 10, 500, 40);
+
+
+        JTextArea DefineBar = new HintTextArea("");
+
+        JScrollPane DefineBarScrollPane = new JScrollPane(DefineBar);
+        DefineBarScrollPane.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+        DefineBar.setRows(20);
+        DefineBar.setColumns(20);
+        midControl.add(DefineBarScrollPane);
+        DefineBar.setFont(font);
+        DefineBar.setBounds(15, 70, 500, 250);
+        //AddWordFrame.add(DefineBar);
+
+
+        AddWordFrame.getContentPane().add(midControl);
+
+
+
+        AddWordFrame.setLocationRelativeTo(null);
+
+
+        AddWordFrame.setVisible(true);
+    }
+
+
+
+
+    public static void main(String[] args) {
         DictionaryApplication dictionary = new DictionaryApplication();
-        dictionary.showExample();
+        dictionary.UI();
     }
 }
