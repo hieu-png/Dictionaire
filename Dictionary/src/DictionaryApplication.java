e6864d8d40e7c2efa017f25de9d2978200fb708f
Mai Cong Danh 19021233 <62579415+wowyman@users.noreply.github.com>
2020-10-03 22:54:46 +0700

    Add files via upload
    update lay du lieu tu file (file chưa format nhé dka) r cat lay tu hien thi ra UI

--- Dictionary/src/DictionaryApplication.java
+++ Dictionary/src/DictionaryApplication.java
@@ -1,30 +1,21 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class DictionaryApplication {
 
     private JFrame mainFrame;
     private JPanel controlPanel;
 
-    //Link hinh anh
-
-    //String imagePath = "C:\\Users\\pc\\OneDrive\\" +
-    //        "Máy tính\\beautiful blue color background\\image.png";
-    String imagePath = "D:\\CocoTit.png";
-
     public DictionaryApplication() {
         prepareGUI();
     }
-
-
 
 
     public void prepareGUI() {
@@ -33,15 +24,13 @@ public class DictionaryApplication {
         mainFrame.getContentPane().setBackground(Color.RED);
         Image image = null;
         try {
-            image = ImageIO.read(new File(imagePath))
+            image = ImageIO.read(new File("C:\\Users\\pc\\OneDrive\\" +
+                    "Máy tính\\beautiful blue color background\\Blue_Background.png"))
                     .getScaledInstance(800, 600, Image.SCALE_DEFAULT);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Panel bg = new Panel();
-
-        mainFrame.setContentPane(bg);
-
+        mainFrame.setContentPane(new ImagePanel(image));
 
 
         mainFrame.setLayout(null);
@@ -60,14 +49,30 @@ public class DictionaryApplication {
 
     }
 
-    public void UI(){
-
+    public void UI(DictionaryApplication dictionaryApplication) {
         mainFrame.setLocationRelativeTo(null);
-        JTextArea searchBar = new HintTextArea("Search...");
 
-        //JLabel label = new JLabel("ssssss");
-        //mainFrame.add(label);
-        //label.setBounds(20,40,100,30);
+        DictionaryManagement d = new DictionaryManagement();
+        ArrayList<String> words = null;
+        try {
+            words = d.insertFromFileAdvanced();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Dictionary dictionary = d.WordFromBigFile(words);
+        int N = dictionary.wordCount();
+        String[] textWord = new String[N];
+        for (int i = 0; i < N; i++) {
+            textWord[i] = dictionary.wordByIndex(i).getText();
+            //System.out.println(textWord[i]);
+        }
+        dictionaryApplication.ShowListMenu(textWord);
+
+
+        JTextField searchBar = new HintTextField("Search...");
+        searchBar.setOpaque(false);
+        searchBar.setForeground(Color.DARK_GRAY);
+
 
         Font f = new Font("Arial", Font.BOLD, 20);
         searchBar.setFont(f);
@@ -76,11 +81,13 @@ public class DictionaryApplication {
 
         Image image = null;
         try {
-            image = ImageIO.read(new File(imagePath))
-                    .getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+            image = ImageIO.read(new File("C:\\Users\\pc\\OneDrive\\" +
+                    "Máy tính\\beautiful blue color background\\book.png"))
+                    .getScaledInstance(18, 18, Image.SCALE_DEFAULT);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         JButton ShowAllWordButton = new JButton();
         assert image != null;
         ShowAllWordButton.setIcon(new ImageIcon(image));
@@ -90,11 +97,11 @@ public class DictionaryApplication {
         ShowAllWordButton.setBounds(0, 50, 28, 28);
 
 
-        JButton OpenAddWordButton = new JButton("Add+");
-        mainFrame.add(OpenAddWordButton);
-        OpenAddWordButton.setBounds(200, 50, 80, 28);
 
-        OpenAddWordButton.addActionListener(new AbstractAction() {
+        JButton AddWordButton = new JButton("Add+");
+        mainFrame.add(AddWordButton);
+        AddWordButton.setBounds(177, 50, 80, 28);
+        AddWordButton.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 AddWordWindow();
@@ -102,85 +109,113 @@ public class DictionaryApplication {
             }
         });
 
+        JButton RemoveButton = new JButton("Remove");
+        mainFrame.add(RemoveButton);
+        RemoveButton.setBounds(256,50,80,28);
+
 
         mainFrame.setVisible(true);
     }
 
 
-
     //------------function display AddWordWindow--------------------------------
 
-//----------------------------------------DKA xem cai layout cua AddBar va DefineBar xem lam sao cho no tach nhau ra y------------------------
-   /* public static void AddWordWindow(){
-        JFrame AddWordFrame = new JFrame("Add new word");
-
-        AddWordFrame.setSize(400, 200);
-
-        JPanel midControl = new JPanel();
-
-        midControl.setSize(400, 200);
-
-        midControl.setLayout(new GridBagLayout());
-
-
-
-
-        Font font = new Font("Arial", Font.BOLD, 14);
-        JTextField AddBar = new HintTextField("Add...");
-
-
-        AddBar.setFont(font);
-        AddBar.setBounds(15, 10, 500, 40);
-
-
-        /*
-        JTextArea DefineBar = new HintTextArea("");
-
-        JScrollPane DefineBarScrollPane = new JScrollPane(DefineBar);
-        DefineBarScrollPane.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
-        DefineBar.setRows(20);
-        DefineBar.setColumns(20);
-       midControl.add(DefineBarScrollPane);
-        DefineBar.setFont(font);
-        DefineBar.setBounds(15, 70, 500, 250);
-        AddWordFrame.add(DefineBar);
-
-
-
-
-        AddWordFrame.getContentPane().add(midControl);
-
-        AddWordFrame.setLocationRelativeTo(null);
-
-
-        AddWordFrame.setVisible(true);
-    }
-    */
     public static void AddWordWindow() {
+
+
         JTextField text = new JTextField(5);
         JTextField definition = new JTextField(5);
 
         //Text and Definition text field component must have the same name
         Object[] components = {
-            new JLabel("Text"),text,
-            new JLabel("Definition"), definition
+                new JLabel("Text"), text,
+                new JLabel("Definition"), definition
         };
 
-       int addWordResult = JOptionPane.showConfirmDialog(null, components,
+        int addWordResult = JOptionPane.showConfirmDialog(null, components,
                 "Enter the word's text and it's definition", JOptionPane.OK_CANCEL_OPTION);
 
-        if (addWordResult == JOptionPane.OK_OPTION) {
-        if (text.getText() != null && definition.getText() != null) {
-            //TODO :ADD WORD
-        }
-        } else {
 
+        if (addWordResult == JOptionPane.OK_OPTION) {
+            if (text.getText() != null) {
+                definition.getText();
+            }//TODO :ADD WORD
         }
     }
 
+    public void ShowListMenu(String[] s) {
+        JList<String> ListWord = new JList<>(s);
+        ListWord.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        ListWord.setVisibleRowCount(20);
+
+        JScrollPane ListScrollPane = new JScrollPane(ListWord);
+        ListScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        mainFrame.add(ListScrollPane);
+        ListScrollPane.setBounds(27, 50, 150, 490);
+
+    }
+
+    public static void Start() {
+        DictionaryApplication dictionaryApplication = new DictionaryApplication();
+        dictionaryApplication.UI(dictionaryApplication);
+
+
+
+    }
 
     public static void main(String[] args) {
-        DictionaryApplication dictionary = new DictionaryApplication();
-        dictionary.UI();
+        Start();
     }
 }
+
+
+//    public static void AddWordWindow() {
+//        JFrame AddWordFrame = new JFrame("Add new word");
+//        AddWordFrame.setSize(600, 400);
+//
+//        JPanel midControl = new JPanel();
+//        midControl.setSize(400, 300);
+//        midControl.setLayout(new BoxLayout(midControl, BoxLayout.Y_AXIS));
+//
+//
+//        //AddWordFrame.getContentPane().setBackground(Color.RED);
+//        //AddWordFrame.setLayout(null);
+////        AddWordFrame.addWindowListener(new WindowAdapter() {
+////            @Override
+////            public void windowOpened(WindowEvent e) {
+////            }
+////        });
+//
+//        Font font = new Font("Arial", Font.BOLD, 14);
+//
+//        JTextField AddBar = new HintTextField("Add...");
+//        AddBar.setOpaque(true);
+//        midControl.add(AddBar);
+//        AddBar.setFont(font);
+//        AddBar.setBounds(15, 10, 500, 40);
+//
+//        JLabel text = new JLabel("Define");
+//        Font font2 = new Font("Arial", Font.BOLD, 10);
+//        text.setFont(font2);
+//        text.setBounds(15, 60, 150, 20);
+//        midControl.add(text);
+//
+//        JTextArea DefineBar = new HintTextArea("");
+//
+//        JScrollPane DefineBarScrollPane = new JScrollPane(DefineBar);
+//        DefineBarScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+//        DefineBar.setRows(20);
+//        DefineBar.setColumns(20);
+//        midControl.add(DefineBarScrollPane);
+//        DefineBar.setFont(font);
+//        DefineBar.setBounds(15, 90, 500, 200);
+//        //AddWordFrame.add(DefineBar);
+//
+//
+//        AddWordFrame.getContentPane().add(midControl);
+//
+//
+//        AddWordFrame.setLocationRelativeTo(null);
+//
+//        AddWordFrame.setVisible(true);
+//    }
--- Dictionary/src/DictionaryManagement.java
+++ Dictionary/src/DictionaryManagement.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.regex.Pattern;
+import java.util.Scanner;
 
 public class DictionaryManagement {
     //Dictionary d = new Dictionary();
 
-//-----NT Hieu-----------22-9------------------------------------
+    //-----NT Hieu-----------22-9------------------------------------
     public void insertFromCommandline(Dictionary d) {
         Scanner in = new Scanner(System.in);
         /*
@@ -18,182 +17,123 @@ public class DictionaryManagement {
         bufferDef=in.nextLine();*/
         d.addWord(in.nextLine(), in.nextLine());
     }
-//-----NT Hieu-----------23-9--------------------------------
+
+    //-----NT Hieu-----------23-9--------------------------------
     public void insertFromFile(Dictionary d, String path) {
 
-    BufferedReader in = null;
-    try {
-        String bufferString;
-
-        in = new BufferedReader(new FileReader(path));
-
-        while ((bufferString = in.readLine()) != null) {
-
-            String[] splitString;
-            splitString = bufferString.split("\t");
-
-            d.addWord(splitString[0],splitString[1]);
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    } finally {
-        try {
-            if (in!=null) {
-                in.close();
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-
-    }
-    //-----NT Hieu-----------28-9--------------------------------
-
-    /**
-     * Database of that file is too large and causing out of memory error. Scrapped.
-     * @param d
-     * @param path
-     */
-    /*
-    public void insertFromFileAdvanced(Dictionary d, String path) {
-    /*
-    BufferedReader in = null;
-    try {
-        String bufferString;
-        String previousString = " ";
-        in = new BufferedReader(new FileReader(path));
-        Word bufferWord = new Word();
-        Word emptyWord = new Word();
-        while ((bufferString = in.readLine()) != null) {
-            if(bufferString.length() > 0 && previousString.length() > 0) {
-            if( (previousString.charAt(0) == '-' || previousString.charAt(0) == '=' )
-            &&bufferString.charAt(0) == '@') {
-                System.out.println("New Word added");
-                d.addWord(bufferWord);
-                bufferWord = emptyWord;
-            }
-                System.out.println(bufferString);
-
-                switch (bufferString.charAt(0)) {
-                case '@' :
-                    bufferWord.setText(bufferString);
-                break;
-
-                case '*' :
-                    bufferWord.setType(bufferString);
-                break;
-
-                case '-' :
-                    bufferWord.addDefinition(bufferString);
-                break;
-                case '=' :
-                    bufferWord.addExample(bufferString);
-                break;
-
-                default :
-                break;
-
-
-            }
-            previousString = bufferString;
-
-        }
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    } finally {
-        try {
-            if (in!=null) {
-                in.close();
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-    }
-
-
-*/
-
-    public void insertFromFileAdvanced(Dictionary d, String path)   {
         BufferedReader in = null;
         try {
-            in = new BufferedReader(new FileReader(path));
             String bufferString;
-           String wordText = "";
-           String wordDef ="";
-            while((bufferString = in.readLine()) != null) {
 
-            if (!bufferString.isEmpty()) {
+            in = new BufferedReader(new FileReader(path));
 
-                if (bufferString.charAt(0) == '@') {
-                    if (wordDef != "") {
-                    d.addWord(wordText,wordDef);
-                   // System.out.println("New word added: " + wordText + "::" + wordDef);
-                        wordDef = "";
-                    }
+            while ((bufferString = in.readLine()) != null) {
 
-                    //bufferString.replaceFirst(" /"," ");
-                   // bufferString.replaceFirst("\n"," ");
-                    int endPoint = bufferString.indexOf(" /");
-                    if (endPoint == -1 ) {
-                        endPoint = bufferString.length();
-                    }
-                    wordText = bufferString.substring(1,endPoint);
+                String[] splitString;
+                splitString = bufferString.split("\t");
 
-
-            } else if (bufferString.charAt(0) == '-' && wordText != "") {
-
-                    wordDef += bufferString;
-                }
-
+                d.addWord(splitString[0], splitString[1]);
             }
-            }
-            //Add the last word
-            d.addWord(wordText,wordDef);
-
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-             try {
-                 if (in != null) {
-                     in.close();
-                 }
-             } catch (IOException e) {
-                 e.printStackTrace();
-             }
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
 
     }
 
-    public static void insertFromFileAdvancedDanh(String path) throws IOException {
+    public ArrayList<String> insertFromFileAdvanced() throws IOException {
+
         StringBuilder s = new StringBuilder();
-        BufferedReader in = new BufferedReader(new FileReader(path));
+        BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\pc\\IdeaProjects\\DictionaryApp\\anhviet109K.txt"));
         ArrayList<String> words = new ArrayList<>();
         String f;
         f = in.readLine();
         f = f.substring(1, f.length());
         String word = "";
         word = word + f + "\n";
-        int i = 0;
-        while (f != null ) {
+
+        String NextText = "";
+        while (f != null) {
+
             while (true) {
+                if (!NextText.isEmpty()) {
+                    word = word + NextText + "\n";
+                    NextText = "";
+                    //System.out.println("-----------------------------word");
+                }
+
                 f = in.readLine();
-                word = word + f + "\n" ;
-                if(f != null)
-                if (!f.isEmpty() && f.charAt(0) == '@') {
+
+
+                //System.out.print(f+'\n');
+                if (f != null)
+                    if (!f.isEmpty() && f.charAt(0) == '@') {
+                        NextText += f;
+                        //System.out.println("---------------------"+NextText);
+
+                        break;
+                    }
+
+                word = word + f + "\n";
+
+                if (f == null) {
+                    word = word.substring(0, word.length() - 5);//substring de xoa \n va null khi f chay den cuoi cung
                     break;
                 }
+
             }
-            System.out.print(word);
+            //System.out.print(word);
             words.add(word);
             word = "";
-        }
 
+
+        }
+        return words;
     }
 
-//-----MC Danh-----------23-9------------------------------------
+    public Dictionary WordFromBigFile(ArrayList<String> words) {
+        ArrayList<String> temp = new ArrayList<>();
+        Dictionary dictionary = new Dictionary();
+        int N = words.size();
+        for (int i = 0; i < N; i++) {
+            int h = words.get(i).indexOf('/');
+            if (h == -1) continue;
+            String s1 = words.get(i).substring(1, h);
+            String s2 = words.get(i).substring(h);
+            dictionary.addWord(s1, s2);
+        }
+//        int n = temp.size();
+//        //return array of textWord
+//        String[] textWord = new String[n];
+//        for(int i=0;i<n;i++){
+//            textWord[i] = temp.get(i);
+//            System.out.println(textWord[i]);
+//        }
+//        return textWord;
+        return dictionary;
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            DictionaryManagement d = new DictionaryManagement();
+            ArrayList<String> words = d.insertFromFileAdvanced();
+            Dictionary dictionary = new Dictionary();
+            d.WordFromBigFile(words);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //-----MC Danh-----------23-9------------------------------------
     //ham tim kiem tra va ve chi so cua String s
     public int dictionaryLookup(ArrayList<Word> arrList, String s) {
 
